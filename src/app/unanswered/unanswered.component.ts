930e8cf37f7c1e2ad974739af4c4f8cb53aac4d2
shrimank <shrimankumbar@gmail.com>
2018-03-28 16:14:04 +0530

    Configuration and unanswered queries pages added

--- src/app/unanswered/unanswered.component.ts
+++ src/app/unanswered/unanswered.component.ts
@@ -7,9 +7,99 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnansweredComponent implements OnInit {
 
+  dialogFlowQueries :any[]=[];
+  lexQueries :any[]=[];
+
   constructor() { }
 
   ngOnInit() {
+    this.dialogFlowQueries.push(...[{
+      query:'Throw my balanace',
+      channel:'facebook',
+      userName:'johnk',
+      channelLog:'fa fa-facebook-official',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'get me balanace',
+      channel:'twitter',
+      userName:'udayt',
+      channelLog:'fa fa-twitter-square',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'Throw my balanace',
+      channel:'facebook',
+      userName:'surya',
+      channelLog:'fa fa-facebook-official',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'get me balanace',
+      channel:'twitter',
+      userName:'karthik',
+      channelLog:'fa fa-twitter-square',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'Throw my balanace',
+      channel:'facebook',
+      userName:'anisht',
+      channelLog:'fa fa-facebook-official',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'get me balanace',
+      channel:'twitter',
+      userName:'karthik',
+      channelLog:'fa fa-twitter-square',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    }
+    ]);
+
+    this.lexQueries.push(...[{
+      query:'Throw my balanace',
+      channel:'facebook',
+      userName:'anithas',
+      channelLog:'fa fa-facebook-official',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'get me balanace',
+      channel:'twitter',
+      userName:'sridharan',
+      channelLog:'fa fa-twitter-square',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'Throw my balanace',
+      channel:'facebook',
+      userName:'rathish',
+      channelLog:'fa fa-facebook-official',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'get me balanace',
+      channel:'twitter',
+      userName:'rajeshk',
+      channelLog:'fa fa-twitter-square',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'Throw my balanace',
+      channel:'facebook',
+      userName:'prashantm',
+      channelLog:'fa fa-facebook-official',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    },
+    {
+      query:'get me balanace',
+      channel:'twitter',
+      userName:'shrimank',
+      channelLog:'fa fa-twitter-square',
+      date:new Date(+(new Date()) - Math.floor(Math.random()*10000000000))
+    }
+    ])
   }
 
 }
