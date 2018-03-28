c51959621a50ae6a5f2b184c6dbe8652162ab0d2
shrimank <shrimankumbar@gmail.com>
2018-03-28 11:38:27 +0530

    Initial Commit.

---
+++ src/app/app.routing.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+import { TestSkillComponent } from './test/test.component';
+import { ConfigureComponent } from './configure/configure.component';
+import { ApisComponent } from './apis/apis.component';
+import { UnansweredComponent } from './unanswered/unanswered.component';
+
+const routes: Routes =[
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'table-list',     component: TableListComponent },
+    { path: 'typography',     component: TypographyComponent },
+    { path: 'icons',          component: IconsComponent },
+    { path: 'maps',           component: MapsComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+    { path: 'upgrade',        component: UpgradeComponent },
+    { path: 'testSkill',      component: TestSkillComponent},
+    { path: 'configure',      component: ConfigureComponent},
+    { path: 'apis',           component: ApisComponent},
+    { path: 'uqueries',       component: UnansweredComponent},
+    { path: '',               redirectTo: 'dashboard', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
