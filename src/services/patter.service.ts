4b4be6181bceda2d0fa3e7871d4258f57242df0d
shrimank <shrimankumbar@gmail.com>
2018-04-04 16:17:51 +0530

    Dashboard Updated with hardcoded data.

--- src/services/patter.service.ts
+++ src/services/patter.service.ts
@@ -13,35 +13,35 @@ export class PatternService {
         'Content-Type': 'application/json'
     });
 
-    constructor(private http: HttpClient,private constants:Constants) {
-        this.patterns=this.getPatterns();
+    constructor(private http: HttpClient, private constants: Constants) {
+        this.patterns = this.getPatterns();
         // this.patterns.push(...[
         //     new Pattern('Account No', '/^\d{10}$/'),
         //     new Pattern('Mobile No', '\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$')
         // ]);
     }
 
-    getPatterns() : Pattern[] {
-        this.http.get(`${this.constants.DIANA_SERVER_URL}/blacklist`).subscribe((allPatterns:Pattern[])=>{
-            console.log("GetPatterns:",allPatterns);
-            this.patterns =  allPatterns;
-            console.log("This patterns:",this.patterns);
+    getPatterns(): Pattern[] {
+        this.http.get(`${this.constants.DIANA_SERVER_URL}/blacklist`).subscribe((allPatterns: Pattern[]) => {
+            console.log("GetPatterns:", allPatterns);
+            this.patterns = allPatterns;
+            console.log("This patterns:", this.patterns);
             this.patternsChanged.next(this.patterns);
             return this.patterns.slice();
         })
         return this.patterns.slice();
-       
-        
+
+
     }
 
     public addPattern(pattern: Pattern) {
 
-        this.http.post(`${this.constants.DIANA_SERVER_URL}/blacklist`,pattern, { headers: this.headers })
-        .subscribe((res)=>{
-            this.patternsChanged.next(this.getPatterns());
-        },err =>{
-            console.log("Error  Response",err)
-        });
+        this.http.post(`${this.constants.DIANA_SERVER_URL}/blacklist`, pattern, { headers: this.headers })
+            .subscribe((res) => {
+                this.patternsChanged.next(this.getPatterns());
+            }, err => {
+                console.log("Error  Response", err)
+            });
     }
 
 
