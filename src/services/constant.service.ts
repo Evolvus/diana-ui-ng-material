e142b9936e2ed2d0984160ab13731d6b121e83ff
shrimank <shrimankumbar@gmail.com>
2018-04-08 14:06:50 +0530

    Request response view

--- src/services/constant.service.ts
+++ src/services/constant.service.ts
@@ -1,8 +1,8 @@
 export class Constants {
 
     
-    //public DIANA_SERVER_URL = 'https://immense-eyrie-81104.herokuapp.com';
-    public DIANA_SERVER_URL = 'http://192.168.1.51:3000';
+    public DIANA_SERVER_URL = 'https://immense-eyrie-81104.herokuapp.com';
+    //public DIANA_SERVER_URL = 'http://192.168.1.51:3000';
 
    
 }
